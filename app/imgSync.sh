c0ecdedaa29af524b1fc12ffb7dcf231d6a89bd4
sv <svrazina@tiestegroup.com>
2014-04-28 17:25:05 +0300

    init develop

--- app/imgSync.sh
+++ app/imgSync.sh
@@ -7,5 +7,6 @@ TARGET_DIR='/var/www/EventWeeklyGraber/upload/event'
 DEST_DIR='/var/www/EventWeekly/public/upload/test2/'
 DEST_HOST='127.0.0.1'
 
+
 echo $(date +'%F %T') ': starting synchronization images with' $DEST_HOST
 rsync --progress --stats --partial --ignore-existing -rv $TARGET_DIR $DEST_DIR > /var/log/EventWeeklyGraber/logs/sync.log
