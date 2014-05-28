8df5d6bb72532686df5efb9f70a1af4e65b208aa
sv <svrazina@tiestegroup.com>
2014-05-28 15:46:19 +0300

    events sync

--- app/dataSync.sh
+++ app/dataSync.sh
@@ -1,34 +1,11 @@
 #!/bin/bash
 
-SCRIPTS=()
-
-check_process() {
-	if [ "$1" = "" ];
-    then
-            return 0
-    fi
-
-    FULL_PATH="$PWD/$1"
-    
-    PROCESS_NUM=$(ps -ef | grep "$FULL_PATH" | grep -v "grep" | grep -v "$$" | wc -l)
-    if [ $PROCESS_NUM -ge 1 ];
-    then
-    	return 1
-    else 
-    	return 0
-    fi
-}
+SCRIPTS=($1)
 
 for SCRIPT_NAME in ${SCRIPTS[*]}
 do
 	FULL_PATH="$PWD/$SCRIPT_NAME"
-	check_process $SCRIPT_NAME
-	CHECK_ANS=$?
-
-	if [ $CHECK_ANS -eq 0 ];
-	then
-		php $FULL_PATH 2>/dev/null &
-	fi
+	php $FULL_PATH 2>/dev/null &
 done
 exit 0
 
