26f0246997a70231a7544a798ac12b00eea36672
sveta <devnull@localhost>
2014-06-16 17:01:08 +0300

    fixes

--- app/tasks/HarvesterTask.php
+++ app/tasks/HarvesterTask.php
@@ -59,27 +59,24 @@ class harvesterTask extends \Phalcon\CLI\Task
 
             if ($query['name'] == 'user_event') {
                 $replacements = array($args[1]);
-                $result = $this -> fb -> getCurlFQL(preg_replace($query['patterns'], $replacements, $query['query']), 
-                									$args[0]);
+                $fql = preg_replace($query['patterns'], $replacements, $query['query']);
+                $result = $this -> fb -> getCurlFQL($fql, $args[0]);
 //print_r($result);                
                 if (count($result -> event) > 0) {
 					$this -> publishToBroker($result, $args, $query['name']);
 				}
-
-                continue;
             }
 
             if ($query['name'] == 'friend_uid') {
                 $replacements = array($args[1]);
-                $result = $this -> fb -> getCurlFQL(preg_replace($query['patterns'], $replacements, $query['query']), 
-                									$args[0]);
+                $fql = preg_replace($query['patterns'], $replacements, $query['query']);
+                $result = $this -> fb -> getCurlFQL($fql, $args[0]);
 //print_r($result);                
                 if (count($result -> friend_info) > 0) {
                 	foreach ($result -> friend_info as $friend) {
                 		$friendsUid[] = json_decode(json_encode($friend), true)['uid2'];
                 	}
                 }
-                continue;
             }
 
             if ($query['name'] == 'friend_event' && isset($friendsUid) && !empty($friendsUid)) {
@@ -89,8 +86,8 @@ class harvesterTask extends \Phalcon\CLI\Task
 
                 do {
                     $replacements = array($start, $limit, $args[1], $fUids);
-                    $result = $this -> fb -> getCurlFQL(preg_replace($query['patterns'], $replacements, $query['query']), 
-                    								$args[0]);
+                    $fql = preg_replace($query['patterns'], $replacements, $query['query']);
+                    $result = $this -> fb -> getCurlFQL($fql, $args[0]);
 //print_r($result);               
                     if (count($result -> event) > 0) {
 						$this -> publishToBroker($result, $args, $query['name']);
@@ -104,8 +101,6 @@ class harvesterTask extends \Phalcon\CLI\Task
                         $start = false;
                     }
                 } while ($start !== false); 
-
-                continue;
             }
 
             if ($query['name'] == 'friend_going_eid' && !empty($friendsUid)) {
@@ -118,7 +113,6 @@ class harvesterTask extends \Phalcon\CLI\Task
                         $friendsGoingUid[] = json_decode(json_encode($event_friend), true)['eid'];
                     }
                 }
-                continue;
             }
 
             if ($query['name'] == 'friend_going_event' && isset($friendsGoingUid) && !empty($friendsGoingUid)) {
@@ -159,8 +153,6 @@ class harvesterTask extends \Phalcon\CLI\Task
                         }
                     }
                 } while ($start !== false);
-
-                continue;
             }
 
             if ($query['name'] == 'user_going_eid') {
@@ -173,7 +165,6 @@ class harvesterTask extends \Phalcon\CLI\Task
                         $userGoingUid[] = json_decode(json_encode($event_user), true)['eid'];
                     }
                 }
-                continue;
             }
 
             if ($query['name'] == 'user_going_event' && isset($userGoingUid) && !empty($userGoingUid)) {
@@ -198,8 +189,6 @@ class harvesterTask extends \Phalcon\CLI\Task
                         $start = false;
                     }
                 } while ($start !== false);
-
-                continue;
             }
 
             if ($query['name'] == 'user_page_uid') {
@@ -212,7 +201,6 @@ class harvesterTask extends \Phalcon\CLI\Task
                         $userPagesUid[] = json_decode(json_encode($user_page), true)['page_id'];
                     }
                 }
-                continue;
             }
 
 
@@ -237,8 +225,6 @@ class harvesterTask extends \Phalcon\CLI\Task
                         $start = false;
                     }
                 } while ($start !== false);
-
-                continue;
             }
 
 
@@ -252,7 +238,6 @@ class harvesterTask extends \Phalcon\CLI\Task
                         $pagesUid[] = json_decode(json_encode($page), true)['page_id'];
                     }
                 }
-                continue;
             }
 
             if ($query['name'] == 'page_event' && isset($pagesUid) && !empty($pagesUid)) {
@@ -278,8 +263,6 @@ class harvesterTask extends \Phalcon\CLI\Task
                         $start = false;
                     }
                 } while ($start !== false);
-
-                continue;
             }
         }
 
