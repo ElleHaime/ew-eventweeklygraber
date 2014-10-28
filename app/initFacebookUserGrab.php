2625962b380e89ba94be83584364e159ff3f9635
sv <devnull@localhost>
2014-10-28 12:41:12 +0200

    separate init for user tasks

---
+++ app/initFacebookUserGrab.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once('init.php');
+
+try {
+	$console -> handle(['task' => 'Tasks\Facebook\User\Observer',
+						'action' => 'observe']);
+		       
+} catch (\Phalcon\Exception $e) {
+        echo $e -> getMessage();
+}
