c0ddfdff8b4675d9b5f9404c4fb44194b895411d
Irbi <svrazina@gmail.com>
2015-08-13 11:19:07 +0300

    fixes

--- app/jobs/Grabber/Sync/Expired.php
+++ app/jobs/Grabber/Sync/Expired.php
@@ -25,15 +25,12 @@ class Expired
 							   -> addQueryCondition('end_date < "' . date('Y-m-d H:i:s') . '"')
 							   -> addQueryFetchStyle('\Models\Event')
 							   -> select();
-var_dump(count($expired) . "\n\r");			
 			if ($expired) {
 				foreach ($expired as $eventObj) {
-print_r($eventObj -> id . " :: " . $eventObj -> end_date . "\n\r");					
 					$grid = new \Models\Event\Grid\Search\Event(['location' => $eventObj -> location_id], $this -> di, null, ['adapter' => 'dbMaster']);
 					$indexer = new \Models\Event\Search\Indexer($grid);
 					$indexer -> setDi($this -> di);
 					if ($indexer -> existsData($eventObj -> id)) {
-print_r($eventObj -> id . " exists in index\n\r");						
 						if (!$indexer -> deleteData($eventObj -> id)) {
 							print_r("ooooooops, " . $eventObj -> id . " not removed from index\n\r");
 						} 
